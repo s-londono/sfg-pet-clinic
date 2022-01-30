9b84e12f2344d806ecaa992adf18e5ca7fa63214
slondono <santiago@b2chat.io>
2022-01-30 18:23:55 -0500

    Implemented Index Controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping({"", "/", "index", "index.html"})
+  public String index() {
+    // Link this method with the index template (index.html)
+    return "index";
+  }
+
+}
